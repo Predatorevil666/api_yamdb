317e50f1edd0e81e43c374d3a3e3d58112def3f8
Sergey Mukhametzhanov <158514131+sealmu98@users.noreply.github.com>
2024-10-17 22:01:24 +0300

    Revert "Comments"

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,8 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
-    'users_reviews.apps.UsersReviewsConfig',
 ]
 
 MIDDLEWARE = [
