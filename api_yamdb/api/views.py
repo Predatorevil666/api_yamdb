01bb20ce02d612fcdf1dd5d020b8a27173675f3a
Alexander Batogov <predatorevil666@mail.ru>
2024-10-19 15:48:52 +0300

    Сделать вьюсеты для API

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,54 @@
+from django.db.models import Avg
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, viewsets, mixins
+from rest_framework.pagination import LimitOffsetPagination
 
 
-# router_v1 = DefaultRouter()
+from api.add_for_view import CreateListDestroyViewSet
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    ReviewSerializer,
+    TitleCreateSerializer,
+    TitleReadSerializer,
+)
+
+
+from reviews.models import (
+    Category,
+    Genre,
+    Title
+)
+
+
+class CategoryViewSet(CreateListDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class GenreViewSet(CreateListDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')).order_by('name')
+    permission_classes = (IsAuthorOrReadOnly)
+    pagination_class = LimitOffsetPagination
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleCreateSerializer
